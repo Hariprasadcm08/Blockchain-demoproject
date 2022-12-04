3a9242b1716333faf3aac15250527ec8fe99f279
Hariprasadcm08 <hariprasadcm08@gmail.com>
2022-12-04 22:30:02 +0530

    final push

--- src/model/cryptoModel.js
+++ src/model/cryptoModel.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose")
 const crptoSchema = new mongoose.Schema({
     symbol: {
         type: String,
-        // unique: true,
+       
     },
     name: {
         type: String,
-        // unique: true,
+      
     },
     marketCapUsd: {
         type: String,
